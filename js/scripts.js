71c6530c9d713314bb8e8192f5a2d16dd874508a
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-10 16:27:28 -0700

    outline tasks and attempt prompt
    co-authoured by: Brian Harvey <brian.harv3y@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// prompt user to write a sentence
+// 1st function take the 1st and last letter and capitilize them
+// 2nd function reverse the order of the 2 letters
+// 3rd function that call the previous 2 functions
+//     add code to 3rd function to concatenate the new two-letter string together with the original phrase
+// 4th function to count the number of letters in the original sentence, divide it by two (rounding down if necessary), and output the letter at this index. 
+//      concatenate this letter at the beginning of the original sentence.
+// Bonus step
+// jQuery
+// bootstrap
+
+const userSentence = prompt("Write a sentance");
+alert outputText = (userSentence).toUpperCase(.charAt(first).charAt(last));
+
+
+
+
+
